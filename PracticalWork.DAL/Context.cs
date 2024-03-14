29ee7b06d3dcd9c4df1149819bbeede2735b6ef7
CRYcombat <koteika5063@gmail.com>
2024-03-14 16:35:15 +0400

    First and second task

--- PracticalWork.DAL/Context.cs
+++ PracticalWork.DAL/Context.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PracticalWork.DAL.Entities;
 
 namespace PracticalWork.DAL
 {
 	public class Context : DbContext
 	{
-		
+		private string _connectionString => @"Data Source=CRYCOMBATPC\MSSQL2019;Initial Catalog=GamesData;TrustServerCertificate=True;Integrated Security=True;Connect Timeout=30;";
+
+		DbSet<Game> Games { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseSqlServer(_connectionString);
+		}
 	}
 }
